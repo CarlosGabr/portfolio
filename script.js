bafc1cd6e685acda6e0ae67b733a633ea25212b3
CarlosGabr <carlosgabrield985@gmail.com>
2025-01-27 20:43:43 -0300

    Retirado nav.

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  const menuMobile = document.querySelector('.cabecalho__menu-mobile');
-  const toggle = document.querySelector('.menu-toggle');
-  menuMobile.classList.toggle('active');
-  toggle.classList.toggle('active');
-}
